b2d7e515c4d13fd3d2cf770b95ce040716110619
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2022-01-14 17:29:40 +0000

    Write tests for diagonal movement functions: `moveUpLeft`, `moveUpRight`, `moveDownLeft`, `moveDownRight`

--- src/__tests__/movement.test.js
+++ src/__tests__/movement.test.js
@@ -63,3 +63,69 @@ describe("Test the movement functions", () => {
     });
   });
 });
+
+describe("Test the diagonal movement functions", () => {
+  describe("The move up-left function", () => {
+    it("should move the robot diagonaly up-left by one position", () => {
+      const currentPosition = { x: 5, y: 5 };
+      const nextPosition = { x: 4, y: 4 };
+
+      expect(moveUpLeft(currentPosition)).toEqual(nextPosition);
+    });
+
+    it("should not cross the boundaries", () => {
+      const currentPosition = { x: 0, y: 0 };
+      const nextPosition = { x: 0, y: 0 };
+
+      expect(moveUpLeft(currentPosition)).toEqual(nextPosition);
+    });
+  });
+
+  describe("The move up-right function", () => {
+    it("should move the robot diagonaly up-right by one position", () => {
+      const currentPosition = { x: 5, y: 5 };
+      const nextPosition = { x: 6, y: 4 };
+
+      expect(moveUpRight(currentPosition)).toEqual(nextPosition);
+    });
+
+    it("should not cross the boundaries", () => {
+      const currentPosition = { x: 9, y: 0 };
+      const nextPosition = { x: 9, y: 0 };
+
+      expect(moveUpRight(currentPosition)).toEqual(nextPosition);
+    });
+  });
+
+  describe("The move down-left function", () => {
+    it("should move the robot diagonaly down-left by one position", () => {
+      const currentPosition = { x: 5, y: 5 };
+      const nextPosition = { x: 4, y: 6 };
+
+      expect(moveDownLeft(currentPosition)).toEqual(nextPosition);
+    });
+
+    it("should not cross the boundaries", () => {
+      const currentPosition = { x: 0, y: 9 };
+      const nextPosition = { x: 0, y: 9 };
+
+      expect(moveDownLeft(currentPosition)).toEqual(nextPosition);
+    });
+  });
+
+  describe("The move down-right function", () => {
+    it("should move the robot diagonaly down-right by one position", () => {
+      const currentPosition = { x: 5, y: 5 };
+      const nextPosition = { x: 6, y: 6 };
+
+      expect(moveDownRight(currentPosition)).toEqual(nextPosition);
+    });
+
+    it("should not cross the boundaries", () => {
+      const currentPosition = { x: 9, y: 9 };
+      const nextPosition = { x: 9, y: 9 };
+
+      expect(moveDownRight(currentPosition)).toEqual(nextPosition);
+    });
+  });
+});
