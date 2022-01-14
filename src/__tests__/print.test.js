e91e7b8f672aca4fb9f4d160753a390709c0e725
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2022-01-14 16:24:08 +0000

    Write test for `printPosition`

--- src/__tests__/print.test.js
+++ src/__tests__/print.test.js
@@ -1,1 +1,10 @@
 const { printPosition } = require("../print");
+
+describe("The print position function", () => {
+  it("should print the string with robot's current position", () => {
+    const currentPosition = { x: 4, y: 3 };
+    const output = `Robot is at { x: ${currentPosition.x}, y: ${currentPosition.y} }`;
+
+    expect(printPosition(currentPosition)).toEqual(output);
+  });
+});
